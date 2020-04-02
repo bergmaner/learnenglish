754a96cc0bbadf4f1c1f7c7a063d47ab4bdc11ed
bergmaner <bergkacper@gmail.com>
2020-04-02 18:35:44 +0200

    Updates

---
+++ precache-manifest.a3d6c5f9d6052baed1db8acefd52997e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28fd6cceafa0c58bbca48a7a2efa76fc",
+    "url": "/learnenglish/index.html"
+  },
+  {
+    "revision": "f268dd0e8e900ef6bf4c",
+    "url": "/learnenglish/static/css/main.849c0931.chunk.css"
+  },
+  {
+    "revision": "e34f14c74d69521d58e1",
+    "url": "/learnenglish/static/js/2.442cd687.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/learnenglish/static/js/2.442cd687.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f268dd0e8e900ef6bf4c",
+    "url": "/learnenglish/static/js/main.7bf0c077.chunk.js"
+  },
+  {
+    "revision": "9cd271bdd4a1eda712e1",
+    "url": "/learnenglish/static/js/runtime-main.c94cc0ee.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/learnenglish/static/media/logo.8dad8028.svg"
+  }
+]);
