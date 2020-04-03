7177ac109a3805b15e8a1c23a8242c1020e17ada
bergmaner <bergkacper@gmail.com>
2020-04-03 23:01:47 +0200

    hooks in Navbar

--- src/features/education/Education.js
+++ src/features/education/Education.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
 import {selectActiveWords} from './educationSlice';
+
 const Education = () =>
 {
     const word = useSelector(selectActiveWords);
     
-return <div><img src ={word.img}/><div>{word.content} - {word.translation}</div></div>
+//return <div className = 'edc'><img src ={word.img}/><div>{word.content} - {word.translation}</div></div>
+return <div></div>
 }
 
 export default Education;
--- src/layout/ResponsiveNavbar/index.js
+++ src/layout/ResponsiveNavbar/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Icon from '@material-ui/core/Icon';
 import styled from 'styled-components';
 
@@ -6,20 +6,21 @@ const ResponsiveToolbar = styled.nav`
 background:#333333;
 height:60px;
 width:100%;
+position:fixed;
 
 `;
 const NavList =  styled.ul`
-display:flex;
+display: flex;
 margin-block-start:0;
 margin-block-end:0;
 margin-inline-start:0;
-height:100%;
+height: 100%;
 align-items:center;
 `;
 
 const ListItem = styled.li`
 list-style-type:none;
-margin:20px 10px;
+padding: 10px 20px;
 `;
 //in Future it will be Link
 const StyledLink = styled.div`
@@ -31,12 +32,25 @@ const StyledLink = styled.div`
   align-items:center;
 `;
 function ResponsiveNavbar({navLinks,background,hoverBackground,linkColor,logo}) {
-  console.log(navLinks);
+
+  const [ hoverIndex, setHoverIndex ] = useState(-1);
+  const [ navOpen, setNavOpen ] = useState(false);
+console.log(navOpen);
     return (
         <ResponsiveToolbar>
             <NavList>
-    {navLinks.map((link,key) => <ListItem>
-        <StyledLink key={key}>{link.text }<Icon style={{fontSize:20,marginRight:10}}>{link.icon}</Icon></StyledLink>
+
+            <figure onClick = {() => setNavOpen(!navOpen)} className = {navOpen ? 'active' : ''}>
+              <Icon style={{color:'palevioletred'}}>menu</Icon>
+            </figure>
+            
+    {navLinks.map((link,index) => 
+    <ListItem
+     key={index}
+     onMouseEnter = {()=> setHoverIndex(index)}
+     onMouseLeave = {() => setHoverIndex(-1)}
+     style = {{background: hoverIndex === index ? '#888' : '',cursor:'pointer'}}>
+        <StyledLink>{link.text }<Icon style={{fontSize:20,marginRight:10}}>{link.icon}</Icon></StyledLink>
     </ListItem>)}
             </NavList>
         </ResponsiveToolbar>
