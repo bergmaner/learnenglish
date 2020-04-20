208f40b177ea69ba7180fc7d65878bb98f732fb9
bergmaner <bergkacper@gmail.com>
2020-04-20 16:49:34 +0200

    Updates

---
+++ precache-manifest.21d8ba963d6c50cb885f488966d32639.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab491fcd2c373323cb88263d28fbf35b",
+    "url": "/learnenglish/index.html"
+  },
+  {
+    "revision": "76105b5d217b417d9412",
+    "url": "/learnenglish/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "ecfac6cefc764014d491",
+    "url": "/learnenglish/static/css/main.2cd6f127.chunk.css"
+  },
+  {
+    "revision": "76105b5d217b417d9412",
+    "url": "/learnenglish/static/js/2.f6a58972.chunk.js"
+  },
+  {
+    "revision": "86414f6aa96d78f04460ff2dda206a29",
+    "url": "/learnenglish/static/js/2.f6a58972.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecfac6cefc764014d491",
+    "url": "/learnenglish/static/js/main.09944b65.chunk.js"
+  },
+  {
+    "revision": "9cd271bdd4a1eda712e1",
+    "url": "/learnenglish/static/js/runtime-main.c94cc0ee.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/learnenglish/precache-manifest.9330518b470083f2ba66f3a4afefd5e0.js"
+  "/learnenglish/precache-manifest.21d8ba963d6c50cb885f488966d32639.js"
 );
 
 self.addEventListener('message', (event) => {
